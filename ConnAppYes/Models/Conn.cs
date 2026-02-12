ad8f2bd4ed71e976bce81ecf2fae14a0d6903f21
Szabi <nagysz@kkszki.hu>
2026-02-12 11:21:23 +0100

    Models

---
+++ ConnAppYes/Models/Conn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnAppYes.Models
+{
+    internal class Conn
+    {
+        private int id;
+        private string nev;
+        private string cim;
+        private string telefon;
+        private string email;
+
+        public Conn(int id, string nev, string cim, string telefon, string email)
+        {
+            this.Id = id;
+            this.Nev = nev;
+            this.Cim = cim;
+            this.Telefon = telefon;
+            this.Email = email;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public string Telefon { get => telefon; set => telefon = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}

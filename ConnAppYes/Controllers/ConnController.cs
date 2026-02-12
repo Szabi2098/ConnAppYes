bd6a45e6a385c2dde50e4d4a54edf3e55857bf4f
Szabi <nagysz@kkszki.hu>
2026-02-12 11:21:11 +0100

    Controller

---
+++ ConnAppYes/Controllers/ConnController.cs
@@ -0,0 +1,38 @@
+﻿using ConnAppYes.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnAppYes.Controllers
+{
+    internal class ConnController
+    {
+        public List<Conn> GetConnList()
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password=;database=connappyes;");
+            conn.Open();
+            string comd = "SELECT * FROM connappyes;";
+            MySqlCommand cmd = new MySqlCommand(comd, conn);
+            List<Conn> connections = new List<Conn>();
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    connections.Add(new Conn(
+                        reader.GetInt32("id"),
+                        reader.GetString("nev"),
+                        reader.GetString("cim"),
+                        reader.GetString("telefon"),
+                        reader.GetString("email")
+                    ));
+                }
+
+                conn.Close();
+                return connections;
+            }
+        }
+    }
+}
